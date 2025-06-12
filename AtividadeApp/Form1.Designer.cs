7109b6ddc7a7825df435469451ad6746cff6837c
Lucas <lcorte110210@gmail.com>
2025-06-12 20:03:12 -0300

    Programando o exercício 6

--- AtividadeApp/Form1.Designer.cs
+++ AtividadeApp/Form1.Designer.cs
@@ -39,18 +39,20 @@
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblLucas = new System.Windows.Forms.Label();
             this.lblPedro = new System.Windows.Forms.Label();
+            this.mstiEx6 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.celsiusParaFahrenheidToolStripMenuItem,
             this.mstiSair});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -61,50 +63,51 @@
             this.mstiEx2,
             this.mstiEx3,
             this.mstiEx4,
-            this.mstiEx5});
+            this.mstiEx5,
+            this.mstiEx6});
             this.celsiusParaFahrenheidToolStripMenuItem.Name = "celsiusParaFahrenheidToolStripMenuItem";
-            this.celsiusParaFahrenheidToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.celsiusParaFahrenheidToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
             this.celsiusParaFahrenheidToolStripMenuItem.Text = "Exercícios";
             // 
             // mstiEx1
             // 
             this.mstiEx1.Name = "mstiEx1";
-            this.mstiEx1.Size = new System.Drawing.Size(180, 22);
+            this.mstiEx1.Size = new System.Drawing.Size(224, 26);
             this.mstiEx1.Text = "Exercício 1";
             this.mstiEx1.Click += new System.EventHandler(this.mstiEx1_Click);
             // 
             // mstiEx2
             // 
             this.mstiEx2.Name = "mstiEx2";
-            this.mstiEx2.Size = new System.Drawing.Size(180, 22);
+            this.mstiEx2.Size = new System.Drawing.Size(224, 26);
             this.mstiEx2.Text = "Exercício 2";
             this.mstiEx2.Click += new System.EventHandler(this.mstiEx2_Click);
             // 
             // mstiEx3
             // 
             this.mstiEx3.Name = "mstiEx3";
-            this.mstiEx3.Size = new System.Drawing.Size(180, 22);
+            this.mstiEx3.Size = new System.Drawing.Size(224, 26);
             this.mstiEx3.Text = "Exercício 3";
             this.mstiEx3.Click += new System.EventHandler(this.mstiEx3_Click);
             // 
             // mstiEx4
             // 
             this.mstiEx4.Name = "mstiEx4";
-            this.mstiEx4.Size = new System.Drawing.Size(180, 22);
+            this.mstiEx4.Size = new System.Drawing.Size(224, 26);
             this.mstiEx4.Text = "Exercício 4";
             this.mstiEx4.Click += new System.EventHandler(this.mstiEx4_Click);
             // 
             // mstiEx5
             // 
             this.mstiEx5.Name = "mstiEx5";
-            this.mstiEx5.Size = new System.Drawing.Size(180, 22);
+            this.mstiEx5.Size = new System.Drawing.Size(224, 26);
             this.mstiEx5.Text = "Exercício 5";
             this.mstiEx5.Click += new System.EventHandler(this.mstiEx5_Click);
             // 
             // mstiSair
             // 
             this.mstiSair.Name = "mstiSair";
-            this.mstiSair.Size = new System.Drawing.Size(41, 20);
+            this.mstiSair.Size = new System.Drawing.Size(48, 24);
             this.mstiSair.Text = "Sair";
             this.mstiSair.Click += new System.EventHandler(this.mstiSair_Click);
             // 
@@ -112,9 +115,10 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(269, 145);
+            this.lblTitle.Location = new System.Drawing.Point(359, 178);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(270, 42);
+            this.lblTitle.Size = new System.Drawing.Size(333, 54);
             this.lblTitle.TabIndex = 1;
             this.lblTitle.Text = "Atividade APP";
             // 
@@ -122,9 +126,10 @@
             // 
             this.lblLucas.AutoSize = true;
             this.lblLucas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLucas.Location = new System.Drawing.Point(23, 328);
+            this.lblLucas.Location = new System.Drawing.Point(31, 404);
+            this.lblLucas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLucas.Name = "lblLucas";
-            this.lblLucas.Size = new System.Drawing.Size(176, 20);
+            this.lblLucas.Size = new System.Drawing.Size(215, 25);
             this.lblLucas.TabIndex = 2;
             this.lblLucas.Text = "Lucas de Comi Côrte";
             // 
@@ -132,23 +137,32 @@
             // 
             this.lblPedro.AutoSize = true;
             this.lblPedro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPedro.Location = new System.Drawing.Point(25, 369);
+            this.lblPedro.Location = new System.Drawing.Point(33, 454);
+            this.lblPedro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPedro.Name = "lblPedro";
-            this.lblPedro.Size = new System.Drawing.Size(137, 20);
+            this.lblPedro.Size = new System.Drawing.Size(166, 25);
             this.lblPedro.TabIndex = 3;
             this.lblPedro.Text = "Pedro Santaella";
             // 
+            // mstiEx6
+            // 
+            this.mstiEx6.Name = "mstiEx6";
+            this.mstiEx6.Size = new System.Drawing.Size(224, 26);
+            this.mstiEx6.Text = "Exercício 6";
+            this.mstiEx6.Click += new System.EventHandler(this.mstiEx6_Click);
+            // 
             // frmMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 397);
+            this.ClientSize = new System.Drawing.Size(1067, 489);
             this.ControlBox = false;
             this.Controls.Add(this.lblPedro);
             this.Controls.Add(this.lblLucas);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
@@ -173,6 +187,7 @@
         private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblLucas;
         private System.Windows.Forms.Label lblPedro;
+        private System.Windows.Forms.ToolStripMenuItem mstiEx6;
     }
 }
 
