e6f9713ee06f16ab6eb92c9fdc98bec16c3c4939
Lucas <lcorte110210@gmail.com>
2025-10-21 17:24:17 -0300

    Programando o exercício 3

--- AtividadeApp/4BimEx3.Designer.cs
+++ AtividadeApp/4BimEx3.Designer.cs
@@ -28,21 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblQtde = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblLargura = new System.Windows.Forms.Label();
+            this.txtQtde = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtLargura = new System.Windows.Forms.TextBox();
+            this.btnCalcularDoWhile = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnCalcularWhile = new System.Windows.Forms.Button();
+            this.btnCalcularFor = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblQtde
+            // 
+            this.lblQtde.AutoSize = true;
+            this.lblQtde.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQtde.Location = new System.Drawing.Point(73, 64);
+            this.lblQtde.Name = "lblQtde";
+            this.lblQtde.Size = new System.Drawing.Size(304, 29);
+            this.lblQtde.TabIndex = 0;
+            this.lblQtde.Text = "Quantidade de Cômodos";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(110, 246);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(121, 29);
+            this.lblArea.TabIndex = 1;
+            this.lblArea.Text = "Área (m²)";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(110, 186);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(126, 29);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura (m)";
+            // 
+            // lblLargura
+            // 
+            this.lblLargura.AutoSize = true;
+            this.lblLargura.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLargura.Location = new System.Drawing.Point(110, 124);
+            this.lblLargura.Name = "lblLargura";
+            this.lblLargura.Size = new System.Drawing.Size(148, 29);
+            this.lblLargura.TabIndex = 3;
+            this.lblLargura.Text = "Largura (m)";
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQtde.Location = new System.Drawing.Point(407, 61);
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(538, 34);
+            this.txtQtde.TabIndex = 4;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Enabled = false;
+            this.txtArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArea.Location = new System.Drawing.Point(407, 243);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(538, 34);
+            this.txtArea.TabIndex = 5;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Enabled = false;
+            this.txtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(407, 183);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(538, 34);
+            this.txtAltura.TabIndex = 6;
+            // 
+            // txtLargura
+            // 
+            this.txtLargura.Enabled = false;
+            this.txtLargura.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLargura.Location = new System.Drawing.Point(407, 121);
+            this.txtLargura.Name = "txtLargura";
+            this.txtLargura.Size = new System.Drawing.Size(538, 34);
+            this.txtLargura.TabIndex = 7;
+            // 
+            // btnCalcularDoWhile
+            // 
+            this.btnCalcularDoWhile.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularDoWhile.Location = new System.Drawing.Point(68, 401);
+            this.btnCalcularDoWhile.Name = "btnCalcularDoWhile";
+            this.btnCalcularDoWhile.Size = new System.Drawing.Size(299, 46);
+            this.btnCalcularDoWhile.TabIndex = 8;
+            this.btnCalcularDoWhile.Text = "Calcular(do... while)";
+            this.btnCalcularDoWhile.UseVisualStyleBackColor = true;
+            this.btnCalcularDoWhile.Click += new System.EventHandler(this.btnCalcularDoWhile_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(738, 401);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(243, 46);
+            this.btnVoltar.TabIndex = 9;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // btnCalcularWhile
+            // 
+            this.btnCalcularWhile.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularWhile.Location = new System.Drawing.Point(68, 333);
+            this.btnCalcularWhile.Name = "btnCalcularWhile";
+            this.btnCalcularWhile.Size = new System.Drawing.Size(299, 46);
+            this.btnCalcularWhile.TabIndex = 10;
+            this.btnCalcularWhile.Text = "Calcular (while)";
+            this.btnCalcularWhile.UseVisualStyleBackColor = true;
+            this.btnCalcularWhile.Click += new System.EventHandler(this.btnCalcularWhile_Click);
+            // 
+            // btnCalcularFor
+            // 
+            this.btnCalcularFor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularFor.Location = new System.Drawing.Point(68, 462);
+            this.btnCalcularFor.Name = "btnCalcularFor";
+            this.btnCalcularFor.Size = new System.Drawing.Size(299, 46);
+            this.btnCalcularFor.TabIndex = 11;
+            this.btnCalcularFor.Text = "Calcular (for)";
+            this.btnCalcularFor.UseVisualStyleBackColor = true;
+            this.btnCalcularFor.Click += new System.EventHandler(this.btnCalcularFor_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(431, 401);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(243, 46);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // frm4BimEx3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.ControlBox = false;
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcularFor);
+            this.Controls.Add(this.btnCalcularWhile);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnCalcularDoWhile);
+            this.Controls.Add(this.txtLargura);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtQtde);
+            this.Controls.Add(this.lblLargura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblQtde);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm4BimEx3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Exercício 3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblQtde;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblLargura;
+        private System.Windows.Forms.TextBox txtQtde;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtLargura;
+        private System.Windows.Forms.Button btnCalcularDoWhile;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnCalcularWhile;
+        private System.Windows.Forms.Button btnCalcularFor;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
--- AtividadeApp/4BimEx3.cs
+++ AtividadeApp/4BimEx3.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,166 @@ namespace AtividadeApp
 {
     public partial class frm4BimEx3 : Form
     {
+        int i = -1;
+        List<double> totalArea = new List<double>();
         public frm4BimEx3()
         {
             InitializeComponent();
+        }
+
+        private void btnCalcularWhile_Click(object sender, EventArgs e)
+        {
+            int qtde = Convert.ToInt32(txtQtde.Text);
+            if(i == -1)
+            {
+                txtQtde.Enabled = false;
+                txtLargura.Enabled = true;
+                txtAltura.Enabled = true;
+                btnCalcularDoWhile.Enabled = false;
+                btnCalcularFor.Enabled = false;
+                txtLargura.Focus();
+                txtArea.Clear();
+                i++;
+            }
+            else
+            {
+                double largura, altura, area;
+                largura = Convert.ToDouble(txtLargura.Text);
+                altura = Convert.ToDouble(txtAltura.Text);
+                area = largura * altura;
+                totalArea.Add(area);
+                txtLargura.Clear();
+                txtAltura.Clear();
+                txtLargura.Focus();
+                i++;
+            }
+            if (i == qtde)
+            {
+                double soma = 0;
+                i = 0;
+                while (i < totalArea.Count)
+                {
+                    soma += totalArea[i];
+                    i++;
+                }
+                txtArea.Text = soma.ToString();
+                i = -1;
+                totalArea.Clear();
+                txtQtde.Enabled = true;
+                txtLargura.Enabled = false;
+                txtAltura.Enabled = false;
+                btnCalcularDoWhile.Enabled = true;
+                btnCalcularFor.Enabled = true;
+            }
+        }
+
+        private void btnCalcularDoWhile_Click(object sender, EventArgs e)
+        {
+            int qtde = Convert.ToInt32(txtQtde.Text);
+            if (i == -1)
+            {
+                txtQtde.Enabled = false;
+                txtLargura.Enabled = true;
+                txtAltura.Enabled = true;
+                btnCalcularWhile.Enabled = false;
+                btnCalcularFor.Enabled = false;
+                txtLargura.Focus();
+                txtArea.Clear();
+                i++;
+            }
+            else
+            {
+                double largura, altura, area;
+                largura = Convert.ToDouble(txtLargura.Text);
+                altura = Convert.ToDouble(txtAltura.Text);
+                area = largura * altura;
+                totalArea.Add(area);
+                txtLargura.Clear();
+                txtAltura.Clear();
+                txtLargura.Focus();
+                i++;
+            }
+            if (i == qtde)
+            {
+                double soma = 0;
+                i = 0;
+                do
+                {
+                    soma += totalArea[i];
+                    i++;
+                } while (i < totalArea.Count);
+                txtArea.Text = soma.ToString();
+                i = -1;
+                totalArea.Clear();
+                txtQtde.Enabled = true;
+                txtLargura.Enabled = false;
+                txtAltura.Enabled = false;
+                btnCalcularWhile.Enabled = true;
+                btnCalcularFor.Enabled = true;
+            }
+        }
+
+        private void btnCalcularFor_Click(object sender, EventArgs e)
+        {
+            int qtde = Convert.ToInt32(txtQtde.Text);
+            if (i == -1)
+            {
+                txtQtde.Enabled = false;
+                txtLargura.Enabled = true;
+                txtAltura.Enabled = true;
+                btnCalcularWhile.Enabled = false;
+                btnCalcularDoWhile.Enabled = false;
+                txtLargura.Focus();
+                txtArea.Clear();
+                i++;
+            }
+            else
+            {
+                double largura, altura, area;
+                largura = Convert.ToDouble(txtLargura.Text);
+                altura = Convert.ToDouble(txtAltura.Text);
+                area = largura * altura;
+                totalArea.Add(area);
+                txtLargura.Clear();
+                txtAltura.Clear();
+                txtLargura.Focus();
+                i++;
+            }
+            if (i == qtde)
+            {
+                double soma = 0;
+                for (i = 0; i < totalArea.Count; i++)
+                    soma += totalArea[i];
+                txtArea.Text = soma.ToString();
+                i = -1;
+                totalArea.Clear();
+                txtQtde.Enabled = true;
+                txtLargura.Enabled = false;
+                txtAltura.Enabled = false;
+                btnCalcularWhile.Enabled = true;
+                btnCalcularDoWhile.Enabled = true;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLargura.Clear();
+            txtAltura.Clear();
+            if (i == -1)
+            {
+                txtQtde.Clear();
+                txtArea.Clear();
+                txtQtde.Focus();
+            }
+            else
+                txtLargura.Focus();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            frmMenu menu = new frmMenu();
+            Hide();
+            menu.Show();
         }
     }
 }
